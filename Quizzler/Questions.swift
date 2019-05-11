1097a0b18571fa5193b4c8fdd8d06a0118beadb5
jkcon <github@xiixii.pw>
2019-05-11 17:03:25 -0500

    Initial commit.

---
+++ Quizzler/Questions.swift
@@ -0,0 +1,20 @@
+//
+//  Questions.swift
+//  Quizzler
+//
+//  Created by James Kip on 5/11/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    
+    let questionText: String
+    let answer : Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+}
